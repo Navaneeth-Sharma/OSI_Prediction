c1ce6d3c301420352e56f746b2d2409ab89fbc5d
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2020-10-05 01:23:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,58 @@ def Encoding_Month(month):
     
     return num
 
-mean = [5218.859029205611,1387.9485510837133,2194.767099990961,
- 0.24628551609082644,14.984616045190817,5.289006914520493]
+def Encoding_Visitor(vis):
+    num = 0
+    if vis == 'Returning Visitor':
+        num = 2
+    elif vis == 'New Visitor':
+        num = 0
+    elif vis == 'Other':
+        num = 1
 
-std = [33825.34232265553,11869.013378146772,15064.67360529188,
- 0.3170625256472935,29.364790319103296,2.354823839699579]
+    return num
+
+def Encoding_Region(region):
+    num = 0
+    if region == 'United States':
+        num = 1
+    elif region == 'Germany':
+        num = 2
+    elif region == 'India':
+        num = 3
+    elif region == 'France':
+        num = 4  
+    elif region == 'United Kingdom':
+        num = 5
+    elif region == 'Brazil':
+        num = 6
+    elif region == 'Italy':
+        num = 7
+    elif region == 'Austalia':
+        num = 8
+    elif region == 'Spain':
+        num = 9
+
+    return num
+
+
+mean = [5228.289588354559,
+        1350.3003767343155,
+        2228.8511406021103,
+        1.6308603553860055,
+        0.04701054003755943,
+        15.070402452799382,
+        3.063447445035639,
+        5.232506776428069]
+
+std =  [33855.16236319768,
+        11714.049416369837,
+        15173.139612558776,
+        0.7571679042941348,
+        0.169662213903642,
+        29.5744024822749,
+        2.3508388396800353,
+        2.3483951485421475]
 
 @app.route("/out",methods=['GET','POST'])
 def Out():
@@ -58,25 +105,28 @@ def features():
         inf_time = request.form.get('inf_time')
         adm_page = request.form.get('adm_page')
         adm_time = request.form.get('adm_time')
-        bounce = request.form.get('bounce')
-        exit1 = request.form.get('exit')
+        visType = request.form.get('visType')
+        spec_day = request.form.get('spec_day')
+        Region = request.form.get('Region')
         pageValue = request.form.get('pageValue')
         month = request.form.get('month')
-        os = request.form.get('os')
 
         pro_page_pr_time = float(pro_page)/(float(pro_time)+0.00001)
         inf_page_pr_time = float(inf_page)/(float(inf_time)+0.00001)
         adm_page_pr_time = float(adm_page)/(float(adm_time)+0.00001)
-        boun_pr_exit = float(bounce)/(float(exit1)+0.00001)
+        visType = Encoding_Visitor(visType)
+        Region = Encoding_Region(Region)
+        month = Encoding_Month(month)
 
-        points = [pro_page_pr_time,adm_page_pr_time,inf_page_pr_time,boun_pr_exit,pageValue]
+
+        points = [pro_page_pr_time,adm_page_pr_time,inf_page_pr_time,visType,spec_day,pageValue,Region,month]
         da = [[float(i)] for i in points]
 
-        da.append([Encoding_Month(month)])
         data = np.asarray(da).T
 
         data = (data - mean)/std
         output = model.predict(data)
+
         if output:
             color = "text-success"
             messege = "Model predicts, The revenue will be Generated"
@@ -85,5 +135,6 @@ def features():
             messege = "Model predicts revenue will not be generated"
         return render_template('out.html',messege = messege, color=color)
     return render_template('out.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
